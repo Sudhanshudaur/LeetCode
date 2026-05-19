ee1c746c2d495e3ad3a2119d01cbdf185016f484
Sudhanshu Daur <daursudhanshu2004@gmail.com>
2026-05-19 19:45:33 +0530

    Update Minimum Common Value.java

--- Easy/Minimum Common Value.java
+++ Easy/Minimum Common Value.java
@@ -1,8 +1,9 @@
 class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
         int i = 0, j = 0;
-        
-        while(i < nums1.length && j < nums2.length) {
+        int len1 = nums1.length;
+        int len2 = nums2.length;
+        while(i < len1 && j < len2) {
             if(nums1[i] == nums2[j]) {
                 return nums2[j];
             } else if (nums1[i] < nums2[j]) {
