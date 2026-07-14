4621b79824c852701cac55be10c253bdc3c4a437
Sudhanshu Daur <daursudhanshu2004@gmail.com>
2026-07-14 19:51:37 +0530

    Create 1189. Maximum Number of Balloons.java

---
+++ Easy/1189. Maximum Number of Balloons.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxNumberOfBalloons(String text) {
+        int[] freq = new int[5];
+        for (char c : text.toCharArray()) {
+            if (c == 'b') {
+                freq[0]++;
+            } else if (c == 'a') {
+                freq[1]++;
+            } else if (c == 'l') {
+                freq[2]++;
+            } else if (c == 'o') {
+                freq[3]++;
+            } else if (c == 'n') {
+                freq[4]++;
+            }
+        }
+        return Math.min(
+                Math.min(freq[0], freq[1]),
+                Math.min(Math.min(freq[2] / 2, freq[3] / 2), freq[4])
+        );
+    }
+}
